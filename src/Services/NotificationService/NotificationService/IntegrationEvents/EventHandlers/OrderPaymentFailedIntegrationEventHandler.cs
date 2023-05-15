c6720fcc2e0af9b28e1f4bdc048dcaf8bacbc18d
Utkan <utkanyl100@gmail.com>
2023-05-15 23:11:31 +0300

    Notifcation Service Added

--- src/Services/NotificationService/NotificationService/IntegrationEvents/EventHandlers/OrderPaymentFailedIntegrationEventHandler.cs
+++ src/Services/NotificationService/NotificationService/IntegrationEvents/EventHandlers/OrderPaymentFailedIntegrationEventHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EventBus.Base.Abstraction;
+using Microsoft.Extensions.Logging;
+using PaymentService.Api.IntegrationEvents.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,20 @@ using System.Threading.Tasks;
 
 namespace NotificationService.IntegrationEvents.EventHandlers
 {
-    internal class OrderPaymentFailedIntegrationEventHandler
+    public class OrderPaymentFailedIntegrationEventHandler : IIntegrationEventHandler<OrderPaymentFailedIntegrationEvent>
     {
+        private readonly ILogger<OrderPaymentFailedIntegrationEventHandler> _logger;
+
+        public OrderPaymentFailedIntegrationEventHandler(ILogger<OrderPaymentFailedIntegrationEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Handler(OrderPaymentFailedIntegrationEvent @event)
+        {
+            //Send Fail Notification (Sms,Email,Push)
+            _logger.LogInformation($"Order Payment failed with OrderId : {@event.OrderId} ,ErrorMessage : {@event.ErrorMessage}");
+            return Task.CompletedTask;
+        }
     }
 }
--- src/Services/NotificationService/NotificationService/IntegrationEvents/EventHandlers/OrderPaymentSuccessIntegrationEventHandler.cs
+++ src/Services/NotificationService/NotificationService/IntegrationEvents/EventHandlers/OrderPaymentSuccessIntegrationEventHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EventBus.Base.Abstraction;
+using Microsoft.Extensions.Logging;
+using PaymentService.Api.IntegrationEvents.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,20 @@ using System.Threading.Tasks;
 
 namespace NotificationService.IntegrationEvents.EventHandlers
 {
-    internal class OrderPaymentSuccessIntegrationEventHandler
+    public class OrderPaymentSuccessIntegrationEventHandler:IIntegrationEventHandler<OrderPaymentSuccessIntegrationEvent>
     {
+        private readonly ILogger<OrderPaymentSuccessIntegrationEventHandler> _logger;
+
+        public OrderPaymentSuccessIntegrationEventHandler(ILogger<OrderPaymentSuccessIntegrationEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Handler(OrderPaymentSuccessIntegrationEvent @event)
+        {
+            //Send Fail Notification (Sms,Email,Push)
+            _logger.LogInformation($"Order Payment success with OrderId : {@event.OrderId} ");
+            return Task.CompletedTask;
+        }
     }
 }
